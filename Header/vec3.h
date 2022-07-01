7bf68285b5786fa9a4d7aff24301fcf4034afd88
Lee <963639875@qq.com>
2022-07-01 11:17:22 +0800

    shadowAcne

--- Header/vec3.h
+++ Header/vec3.h
@@ -47,6 +47,28 @@ public:
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    inline static double random_double()
+    {
+        // Returns a random real in [0,1).
+        return rand() / (RAND_MAX + 1.0);
+    }
+
+    inline static double random_double(double min, double max)
+    {
+        // Returns a random real in [min,max).
+        return min + (max - min) * random_double();
+    }
+
+    inline static vec3 random()
+    {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max)
+    {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
 public:
     double e[3];
 
@@ -95,6 +117,19 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere()
+{
+    while(true)
+    {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1)
+        {
+            continue; // 进行下一次循环
+        }
+        return p;
+    }
 }
 
 // Type aliases for vec3
--- main.cpp
+++ main.cpp
@@ -7,44 +7,22 @@
 #include "Header/camera.h"
 #include <iostream>
 
-// // 返回与球的两个交点t
-// double hit_sphere(const point3 &center, double radius, const ray &r)
-// {
-//     vec3 oc = r.origin() - center;
-//     auto a = r.direction().length_squared();
-//     auto half_b = dot(oc, r.direction());
-//     auto c = oc.length_squared() - radius * radius;
-//     auto discriminant = half_b * half_b - a * c;
-//     if (discriminant < 0)
-//     {
-//         return -1.0;
-//     }
-//     else
-//     {
-//         return (-half_b - sqrt(discriminant) ) / a;
-//     }
-// }
-
-// color ray_color(const ray &r)
-// {
-//     auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-//     if (t > 0.0)
-//     {
-//         vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-//         return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-//     }
-//     vec3 unit_direction = unit_vector(r.direction());                   // 计算单位向量
-//     t = 0.5 * (unit_direction.y() + 1.0);                               // 将y值归一化
-//     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0); // 线性插值
-// }
-
-color ray_color(const ray &r, const hittable &world)
+color ray_color(const ray &r, const hittable &world, int depth)
 {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec))
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
     {
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        return color(0,0,0);
     }
+
+    if (world.hit(r, 0.001, infinity, rec))
+    {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -57,6 +35,7 @@ int main()
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     // World
     hittable_list world;
@@ -79,7 +58,7 @@ int main()
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
