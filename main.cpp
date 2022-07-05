8e2430164c462cbd64ecb877ab1f7c4051a98a8a
Lee <963639875@qq.com>
2022-07-05 21:57:15 +0800

    Mixture

--- main.cpp
+++ main.cpp
@@ -35,10 +35,13 @@ color ray_color(
     color albedo;
     if (!rec.mat_ptr->scatter(r, rec, albedo, scattered, pdf_val))
         return emitted;
+    
+    auto p0 = make_shared<hittable_pdf>(lights, rec.p);
+    auto p1 = make_shared<cosine_pdf>(rec.normal);
+    mixture_pdf mixed_pdf(p0, p1);
 
-    hittable_pdf light_pdf(lights, rec.p);
-    scattered = ray(rec.p, light_pdf.generate(), r.time());
-    pdf_val = light_pdf.value(scattered.direction());
+    scattered = ray(rec.p, mixed_pdf.generate(), r.time());
+    pdf_val = mixed_pdf.value(scattered.direction());
 
     return emitted
          + albedo * rec.mat_ptr->scattering_pdf(r, rec, scattered)
